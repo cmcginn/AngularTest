c9b12c175d3c4c34555503055d30f81c2a61e930
Chris McGinn <chris.s.mcginn@live.com>
2017-11-12 14:45:49 -0500

    before switching angular to CDN

--- AngularTest/App_Start/BundleConfig.cs
+++ AngularTest/App_Start/BundleConfig.cs
@@ -9,20 +9,25 @@ namespace AngularTest
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/vendor/jquery-{version}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/vendor/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/vendor/bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            //bundles.Add(new ScriptBundle("~/bundles/angular").Include("~/Scripts/vendor/angular.js",
+            //    "~/Scripts/vendor/angular-*"));
+
+            //bundles.Add(new ScriptBundle("~/bundles/app")
+            //    .IncludeDirectory("~/Scripts/app", "*.js", true));
         }
     }
 }
---
+++ AngularTest/Scripts/app/app.js
@@ -0,0 +1,10 @@
+﻿'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+        
+]).config([function ($locationProvider) {
+        $locationProvider.hashPrefix('!');
+
+       // $routeProvider.otherwise({ redirectTo: '/view1' });
+}]);
